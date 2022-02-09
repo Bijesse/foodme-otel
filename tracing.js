5926d47cbfad7ba8140ead8a1809440d8f5ef9ec
Tom Bijesse <tom@bijesse.com>
2022-02-09 11:19:23 -0800

    updated comment

--- tracing.js
+++ tracing.js
@@ -1,1 +1,1 @@
-// Replace this comment with the code linked on Lesson 5 of the Introduction to OpenTelemerty course here
+// Replace this comment with the code linked on Lesson 5 of the Introduction to OpenTelemerty course
